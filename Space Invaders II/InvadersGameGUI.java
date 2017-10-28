3a55919a4117349c0ee758eaf441e1a871e2a0d2
simratv <32748426+simratv@users.noreply.github.com>
2017-10-27 18:44:33 -0600

    Add files via upload

---
+++ Space Invaders II/InvadersGameGUI.java
@@ -0,0 +1,154 @@
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Color;
+
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.BoxLayout;
+import javax.swing.*;
+
+import java.awt.event.*;
+import java.awt.Dimension;
+
+/**
+* This class acts as the controller for the GUI version of the Invaders Game. It responds to
+* keyboard input and timer events.
+*/
+
+public class InvadersGameGUI implements KeyListener , Runnable{
+	private boolean shipRight;
+	private boolean shipLeft;
+    private int windowWidth = 400;
+    private int windowHeight = 500;
+
+
+    private playerShot shot = new playerShot(350, 5, 10, 30);
+    private playerShip ship = new playerShip(windowWidth, 3);
+    private Alien alien1= new Alien();
+    private InvadersGameScreen screen = new InvadersGameScreen();
+	
+	public InvadersGameGUI(){
+		init();
+	}
+    public void init() {
+        screen.addKeyListener(this);
+		new Thread(this).start();
+		
+    }
+
+	
+    public void update(double timeDiffer) {
+		if (shipRight){
+			ship.moveRight(timeDiffer);
+		}
+		
+		if (shipLeft){
+			ship.moveLeft(timeDiffer);
+		}
+    }
+	
+
+    // Keyboard event handling
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == 'F' && !shot.getShotFired()) { // Press F to make a shot
+            shot.shotFired(true);
+        } else if (e.getKeyCode() == KeyEvent.VK_SPACE && shot.getShotFired()) { // For now, move the bullet up with space
+            shot.moveShot();
+            shot.inBounds();
+        }
+		if (e.getKeyCode() == KeyEvent.VK_A){
+			shipLeft = true;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_D){
+			shipRight = true;
+		}
+
+            
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_A){
+			shipLeft = false;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_D){
+			shipRight = false;
+		}
+	}
+
+    @Override
+    public void keyTyped(KeyEvent e) {}
+
+    /****** GUI interface ******/
+
+    private class InvadersGameScreen extends JFrame {
+
+        public InvadersGameScreen() {
+
+            super("Space Invaders Game");
+            super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            this.setLocationRelativeTo(null);
+
+            Canvas canvas = new Canvas();
+
+            /* Code from Stack Overflow answer (https://stackoverflow.com/questions/
+            * 6593322/why-does-the-jframe-setsize-method-not-set-the-size-correctly) */
+            canvas.setPreferredSize(new Dimension(windowWidth, windowHeight));
+            this.getContentPane().add(canvas);
+            this.pack();
+
+            this.setVisible(true);
+        
+        }
+
+        private class Canvas extends JComponent{
+			
+			
+
+            @Override
+            public void paintComponent(Graphics g){
+
+                // Draw background
+                g.setColor(Color.BLACK);
+                g.fillRect(0,0,windowWidth,windowHeight);
+
+                // Draw aliens
+                int xcoord = alien1.getAlienX();
+                int ycoord= alien1.getAlienY();
+
+                g.setColor(Color.GREEN);
+                g.fillOval(xcoord,ycoord,45,40);
+
+                // Draw ship
+				ship.draw(g);
+
+                // Draw bullet
+                if (shot.getShotFired()) {
+                    shot.draw(g);
+                }
+
+            }
+    
+        }
+
+    }
+
+		@Override
+        public void run(){
+			double time = System.currentTimeMillis();
+			
+			try{Thread.sleep(2);}
+			catch(Throwable t){}
+			double timeDiffer = (System.currentTimeMillis()-time);
+			time = System.currentTimeMillis();
+			update(timeDiffer);
+			screen.repaint();
+			
+			
+		}
+	public static void main(String[] args){
+		 InvadersGameGUI game = new InvadersGameGUI();
+	}
+			
+}
---
+++ Space Invaders II/playerShip.java
@@ -0,0 +1,76 @@
+import java.awt.*;
+
+public class playerShip{
+/*************************************************
+This class holds the method mechanics of the player ship
+*************************************************/  
+
+    private int location = 30;
+    private int lastLocation = location;
+    private int speed = 5; //pixels
+
+    /**
+    * Constructor that centers the starting location of the ship based on the length of the screen and lets
+    * one set its speed.
+    */
+    public playerShip(int screenLength, int newSpeed) {
+        location = screenLength/2;
+        speed = newSpeed;
+    }
+
+
+    /****************************
+    method: setLocation
+            sets new horizontal index position of the ship
+    @param  index position for player ship to be given
+    ****************************/
+    public void setLocation(int newLocation) {
+        lastLocation = location;
+        location = newLocation;
+    }
+    
+    /****************************
+    method: getSpeed
+    @return amount of characters that the player ship can move left or right
+    ****************************/
+    public int getSpeed() {
+        return speed;
+    }
+    
+    /****************************
+    method: getLocation
+            checks if horizontal index position of player ship is out of bounds
+            on the board array's horizontal parameter
+    ****************************/
+	public int getLocation() {
+        return location;
+    }
+	
+	
+	
+    public void inBounds(int boardWidth) { 
+        if (location >= boardWidth) {
+            location = boardWidth - speed;
+        } else if (location < speed) {
+            location = 0;
+        }
+    }
+    
+    /****************************
+    method: 
+            moves the player ship speed characters left or right depending on user input
+    @param left or right direction the player ship is to go
+    ****************************/
+    public void moveRight(double timeDiffer) {
+        location+=timeDiffer*speed;
+	}
+	
+	public void moveLeft(double timeDiffer){
+		location-=timeDiffer*speed;
+	}
+	public void draw(Graphics g){
+		int xcoordShip = getLocation();
+        g.setColor(Color.WHITE);
+        g.fillRect(xcoordShip-25,450,50,50);
+	}
+}
