d1b6531c0df71856298d7386d9e6b611d41a25bb
insomniman <32779870+insomniman@users.noreply.github.com>
2017-10-30 16:04:35 -0600

    Add files via upload

--- SpaceInvaders/g00dVersions/InvadersGame.java
+++ SpaceInvaders/g00dVersions/InvadersGame.java
@@ -1,9 +1,11 @@
 /******************************************************************************
                             Group 13: Demo 1 Invaders Game
                             
-This program launches a text-based, simple version of Invaders Game. There is one 
-player ship, one alien, with the ability for the playership to move left or right
-or shoot by user input across a board.
+This program launches a text-based, incomplete version of Invaders Game. There is one 
+player-controlled ship, now with numerous rows and columns of aliens moving from right
+to left down the board, with the player being able to shoot and destroy them until
+they win or lose the game depending on if aliens reach the bottom of the board. 
+Aliens do not shoot (yet).
 ******************************************************************************/
 
 import java.util.Scanner;
@@ -20,6 +22,10 @@ public class InvadersGame{
     
     char[][] board = new char[boardHeight][boardWidth]; 
 
+	/******************************************************
+	method: play
+			begins the game itself, creating a board and updating it until player quits or loses
+	******************************************************/
     public void play(){
         createBoard();
         
@@ -29,7 +35,10 @@ public class InvadersGame{
             quitCondition();
         }
     }
-    
+    /******************************************************
+	method: createBoard
+			creates a board of a double array with clear height and width dimensions
+	******************************************************/
     public void createBoard(){
         for (int r = 0; r < boardHeight; r++) {
             for (int c = 0; c < boardWidth; c++) {
@@ -37,7 +46,10 @@ public class InvadersGame{
             }
         }  
     }
-
+	/******************************************************
+	method: printBoard
+			prints each index of the board array onto screen
+	******************************************************/
     public void printBoard(){
         for (int r = 0; r < boardHeight; r++) {
             System.out.print("|"); 
@@ -48,6 +60,10 @@ public class InvadersGame{
         }
     }
     
+	/******************************************************
+	method: drawShip
+			prints the location of the player ship on the board
+	******************************************************/
     public void drawShip(){
         board[boardHeight-1][ship.getLocation()] = 'X';
         if (ship.getLocation() != ship.getLastLocation()) {
@@ -55,6 +71,14 @@ public class InvadersGame{
         }
     }
     
+	/******************************************************
+	method: drawShot
+			draws the current player shot on the board, checking if the player shot hit an alien,
+			destroying the alien if so and setting shotFired (trigger for the shot) to false, thus
+			"removing" the shot from the board
+			checks if shot is within the bounds of the board
+			replaces last shot position on board with a space character
+	******************************************************/
     public void drawShot(){
         for (int r = 0; r < alienInvaders.getRowsAliens() ; r++) {
 			for (int c=0; c<alienInvaders.getNumAliens();c++){
@@ -77,7 +101,11 @@ public class InvadersGame{
         }
     }
 
-
+	/******************************************************
+	method: drawAliens
+			draws array of aliens on the board, replacing last alien positions with spaces first
+			and then setting the new alien positions if they are still "alive"
+	******************************************************/
     public void drawAliens(){ 
 		for (int r = 0; r < alienInvaders.getRowsAliens() ; r++) {
 			for (int c=0; c<alienInvaders.getNumAliens();c++){
@@ -92,13 +120,24 @@ public class InvadersGame{
         }
     }
 
+	/******************************************************
+	method: drawCurrentState
+			draws the current iteration of the game onto the board
+	******************************************************/
     public void drawCurrentState(){ 
         drawShip();
         drawShot();
         drawAliens();
         printBoard();
     }  
-       
+    
+	/******************************************************
+	method: handleShot
+			sets player shot column position to the ship's colum position and sets
+			shotFired to true (meaning a shot exists on the board)
+			if shot already exists, calls to move the shot up the board
+	@param  handlePart if set to "part1", is meant to initialize the player shot
+	******************************************************/
     public void handleShot(String handlePart){
         if (handlePart == "part1"){
             if (!shot.getShotFired()) { 
@@ -111,7 +150,12 @@ public class InvadersGame{
             shot.moveShot();
         }   
     }
-    
+	
+    /******************************************************
+	method: handleEvents
+			checks for player input; whether player entered to move, shoot, or quit
+			also calls to move the alien array
+	******************************************************/
     public void handleEvents(){
         Scanner keyboard = new Scanner(System.in);
         System.out.print("Enter A for left, D for right, or F to shoot (Q to quit)"); 
@@ -134,7 +178,12 @@ public class InvadersGame{
         alienInvaders.aliensMovement(boardWidth - 4);
     }
 	
-	
+	/******************************************************
+	method: quitCondition
+			checks whether or not all aliens have been destroyed, giving the player the win
+			or if first alien on first rows reached the row before the last on board, giving 
+			the player the loss
+	******************************************************/
     public void quitCondition(){
 		int count=0;
         if (alienInvaders.aliens[alienInvaders.getRowsAliens() -1][1].inBounds(boardHeight)) {
