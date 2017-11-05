b12c5b8f22df6309056f04a7d8cbf8572c38c261
LynCM <celinama126@gmail.com>
2017-11-05 14:13:33 -0700

    Add files via upload
    Set up inheritance for the ship class using a parent Shape class which holds coordinates, speed and size info; I didn't use the Point class so that getting x/y coordinates would be easier but the Shape class ends up being pretty long this way

--- SpaceInvaders/inprogressVersions/mergingthoughts/playerShip.java
+++ SpaceInvaders/inprogressVersions/mergingthoughts/playerShip.java
@@ -2,81 +2,43 @@
 import java.awt.Graphics;
 import java.awt.Color;
 
-public class playerShip{
+public class playerShip extends Shape{
 /*************************************************
 This class holds the method mechanics of the player ship
 *************************************************/  
-
-    private int location = 30;
-    private int lastLocation = location;
-    private int speed = 3;
 
     /*****************************************************************
     * Constructor that centers the starting location of the ship based on the length of the screen and lets
     * one set its speed.
     *****************************************************************/
-    public playerShip(int screenLength, int newSpeed) {
-        location = screenLength/2;
-        speed = newSpeed;
-    }
-
-    /****************************
-    method: getLocation
-    @return current horizontal index of the player ship
-    ****************************/
-    public int getLocation() {
-        return location;
-    }
-
-    /****************************
-    method: getLastLocation
-    @return previous horizontal index of the player ship
-    ****************************/
-    public int getLastLocation() {
-        return lastLocation;
-    }
-
-    /****************************
-    method: setLocation
-            sets new horizontal index position of the ship
-    @param  index position for player ship to be given
-    ****************************/
-    public void setLocation(int newLocation) {
-        lastLocation = location;
-        location = newLocation;
+    public playerShip(int screenLength, int newY, int size, int newSpeed) {
+    	super(screenLength/2, newY, size, size);
+        setHSpeed(newSpeed);
     }
     
     /****************************
-    method: getSpeed
-    @return amount of characters that the player ship can move left or right
-    ****************************/
-    public int getSpeed() {
-        return speed;
-    }
-    
-    /****************************
-    method: getLocation
+    method: inBounds
             checks if horizontal index position of player ship is out of bounds
             on the board array's horizontal parameter
     ****************************/
     public void inBounds(int boardWidth) { 
-        if (location >= boardWidth) {
-            location = boardWidth - speed;
-        } else if (location < speed) {
-            location = 0;
+        if (getXCoord() >= boardWidth) {
+            setXCoord(boardWidth - getHSpeed());
+        } else if (getXCoord() < getHSpeed()) {
+            setXCoord(0);
         }
     }
     
     // Condensed ship movement to 1 method
     
     public void move(String direction) {
-    	lastLocation = location;
     	if (direction.equals("A")) { // If moving left, subtract speed
-    		location -= speed;
+    		setXCoord(getXCoord()-getHSpeed());
     	} else if (direction.equals("D")) {
-    		location += speed;
+    		setXCoord(getXCoord()+getHSpeed());
     	}
     }
+    
     
 	/**********************************
 	method: draw
@@ -85,6 +47,6 @@ This class holds the method mechanics of the player ship
 	*************************************/
 	public void draw(Graphics g) {
         g.setColor(Color.WHITE);
-        g.fillRect(location-10,440, 20, 20);
+        g.fillRect(getXCoord()-10,getYCoord(), getWidth(), getHeight());
     }
 }
