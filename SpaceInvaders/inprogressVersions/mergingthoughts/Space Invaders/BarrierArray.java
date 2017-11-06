1d097586a71b97e0182595b3972f18605ed8f025
simratv <32748426+simratv@users.noreply.github.com>
2017-11-06 11:12:54 -0700

    Add files via upload

---
+++ SpaceInvaders/inprogressVersions/mergingthoughts/Space Invaders/BarrierArray.java
@@ -0,0 +1,17 @@
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Color;
+
+
+public class BarrierArray{
+    
+	
+    private int numAliens;
+    private int rowsAliens;
+    Barrier[][] barriers = new Barrier[3][1];
+	private boolean moveRight = true;
+
+    
+	
+	   
+}
