ac4c3ed3a6d733787ca09a7768d0748522a5e8f9
simratv <32748426+simratv@users.noreply.github.com>
2017-11-06 00:47:38 -0700

    Update playerShip.java

--- SpaceInvaders/inprogressVersions/mergingthoughts/playerShip.java
+++ SpaceInvaders/inprogressVersions/mergingthoughts/playerShip.java
@@ -1,26 +1,26 @@
-
 import java.awt.Graphics;
 import java.awt.Color;
 
 public class playerShip extends Shape{
-/*************************************************
-This class holds the method mechanics of the player ship
-*************************************************/  
+	/**
+	* This class holds the method mechanics of the player ship
+	*/  
 
-    /*****************************************************************
-    * Constructor that centers the starting location of the ship based on the length of the screen and lets
+    /** Constructor that centers the starting location of the ship based on the length of the screen and lets
     * one set its speed.
+	* @param screenLength is the length of the screen
+	* @param newY is the y coordinate of the ship
+	* @param size is the size of the ship
+	* @newSpeed the horizontal speed of the ship
     *****************************************************************/
     public playerShip(int screenLength, int newY, int size, int newSpeed) {
     	super(screenLength/2, newY, size, size);
         setHSpeed(newSpeed);
     }
     
-    /****************************
-    method: inBounds
-            checks if horizontal index position of player ship is out of bounds
-            on the board array's horizontal parameter
-    ****************************/
+    /**This method checks if horizontal index position of player ship is out of bounds
+    * on the board array's horizontal parameter
+    */
     public void inBounds(int boardWidth) { 
         if (getXCoord() >= boardWidth) {
             setXCoord(boardWidth - getHSpeed());
@@ -29,10 +29,12 @@ This class holds the method mechanics of the player ship
         }
     }
     
-    // Condensed ship movement to 1 method
+    /** This method moves the ship left or right horizontally depending on the string input by the player
+	* @param is the string input that decides which way the ship moves
+	*/
     
     public void move(String direction) {
-    	if (direction.equals("A")) { // If moving left, subtract speed
+    	if (direction.equals("A")) { 
     		setXCoord(getXCoord()-getHSpeed());
     	} else if (direction.equals("D")) {
     		setXCoord(getXCoord()+getHSpeed());
@@ -40,11 +42,9 @@ This class holds the method mechanics of the player ship
     }
     
     
-	/**********************************
-	method: draw
-			draws the ship
-			@param Graphics object,g
-	*************************************/
+	/** This method draws the ship
+	@param Graphics object,g
+	*/
 	public void draw(Graphics g) {
         g.setColor(Color.WHITE);
         g.fillRect(getXCoord()-10,getYCoord(), getWidth(), getHeight());
