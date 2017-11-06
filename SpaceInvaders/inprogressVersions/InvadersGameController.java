ebedd55fed0ca792d8bbc7f2c7ad6f7a874dfbe3
insomniman <32779870+insomniman@users.noreply.github.com>
2017-11-05 22:20:31 -0700

    documented

--- SpaceInvaders/inprogressVersions/InvadersGameController.java
+++ SpaceInvaders/inprogressVersions/InvadersGameController.java
@@ -6,10 +6,15 @@ import java.awt.event.KeyListener;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-//this class contains all ACTION events. The controller gives actions to the logic, 
-//the logic updates locations and passes it back to controller, the controller
-//then passes it to GUI which draws the locations. the process then repeats
-
+/*
+This class plays either the text version or GUI version of the game. It contains all ACTION events.
+If the GUI version is chosen by the user in main, the controller calls actions to the gui version
+constructor of the logic, which updates locations and passes it back to controller, the controller then 
+passes it to GUI which draws the locations. This process repeats until a win or loss.
+If the text version is chosen, the controller calls the text version constructor of the logic, initalizes
+the class that prints the text board (InvadersGameText), and then plays the text version of the game 
+using calls to print and update ship/shot/alien variables through the logic class and text class
+*/
 public class InvadersGameController implements KeyListener{
     
 	private Scanner keyboard = new Scanner(System.in);
@@ -33,9 +38,11 @@ public class InvadersGameController implements KeyListener{
     	}
     }
     
-    /**
-    *  This method starts and plays the GUI version of the game.
-    */
+    /*********
+    method: playText
+			This method starts and plays the GUI version of the game. Handling win/loss conditions and movement of
+			aliens and shots in the Timer and the movement of the ship outside the timer using a key listener.
+    *********/
     public void playGui(){
     	
         Timer timer = new Timer(40, new ActionListener(){
@@ -53,8 +60,15 @@ public class InvadersGameController implements KeyListener{
         timer.start();
     	gui.addKeyListener(this);
     }
-    
-    // Since there are some of the same steps in both of these "play" methods we can probably reduce code repetition more
+	
+	/******
+	method: playText
+			Plays the text version of the game. Uses conditionals to take input from user, calling 
+			methods from the logic class depending on the input to update ship/shot and checking for
+			a quit input. Calls methods from logic class to update aliens and shot interactions,
+			and calls a method (drawCurrentState) from InvadersGameText to draw the board onto screen
+				
+	*******/
     
     public void playText() { // Plays text version of the game, need to implement board printing
     	boolean quit = false;
@@ -80,11 +94,12 @@ public class InvadersGameController implements KeyListener{
     	}
      }
     
-     /**
-    *  This method responds to keyboard input and updates the screen. When the user presses A/D, the ship moves left or right.
-    *  When the user presses the space key, a bullet will be fired if one is not already active.
-    *  @param  e  a key pressed by the user
-    */
+     /********
+    method: keyPressed
+	*		This method responds to keyboard input and updates the screen. When the user presses A/D, the ship moves left or right.
+    *  		When the user presses the space key, a bullet will be fired if one is not already active.
+    *  		@param  e  a key pressed by the user
+    *******/
 
     @Override
     public void keyPressed(KeyEvent e) {
