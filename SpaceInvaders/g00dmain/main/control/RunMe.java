d3058684d6c2647df994d492370360703972028f
LynCM <celinama126@gmail.com>
2017-12-08 12:36:26 -0700

    documentation

--- SpaceInvaders/g00dmain/main/control/RunMe.java
+++ SpaceInvaders/g00dmain/main/control/RunMe.java
@@ -1,14 +1,12 @@
 package control;
 
+/**
+ * This class contains the main method that creates and runs an instance of InvadersGameController,
+ * which will launch the game.
+ */
 class RunMe {
     
     public static void main (String[] args){
         javax.swing.SwingUtilities.invokeLater(InvadersGameController::new);
     }
 }
-/*this class takes action input from the controller, updates the objects, and then
-	 passes those updated objects back to controller.
-	 @param screenHeight, screenWidth are the dimensions of the board
-	* @param gameStatus lets the program know if the game should continue or not.
-	* @param gameVersion lets the constructor know whether the text and GUI wasnt 
-	*/
